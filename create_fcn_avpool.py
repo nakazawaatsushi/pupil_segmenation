6d8bc3709ce24db4da65cb0dc035fd1b51a2cadc
nakazawa atsushi <nakazawa.atsushi@gmail.com>
2018-01-31 21:01:18 +0000

    added augumented results

---
+++ create_fcn_avpool.py
@@ -0,0 +1,109 @@
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Concatenate, Flatten, Dense
+from keras.layers import Dropout, AveragePooling2D
+from keras.layers import merge
+
+def create_fcn00(input_size):
+    inputs = Input((3, input_size[1], input_size[0]))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv1_1')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv1_2')(conv1)
+    pool1 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv2_1')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv2_2')(conv2)
+    pool2 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv3_1')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv3_2')(conv3)
+    pool3 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv4_1')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv4_2')(conv4)
+    pool4 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv5_1')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv5_2')(conv5)
+    pool5 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv5)
+
+    conv6 = Conv2D(1024, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv6_1')(pool5)
+    conv6 = Conv2D(1024, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv6_2')(conv6)
+
+    up7 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv6), conv5])
+    conv7 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up5_1')(up7)
+    conv7 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up5_2')(conv7)
+
+    up8 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv7), conv4])
+    conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up4_1')(up8)
+    conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up4_2')(conv8)
+
+    up9 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv8), conv3])
+    conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up3_1')(up9)
+    conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up3_2')(conv9)
+
+    up10 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv9), conv2])
+    conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up2_1')(up10)
+    conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up2_2')(conv10)
+
+    up11 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv10), conv1])
+    conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up1_1')(up11)
+    conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up1_2')(conv11)
+
+    conv12 = Conv2D(1, (1, 1), activation='sigmoid', padding='same', data_format='channels_first', name='conv_fin')(conv11)
+    #conv12 = Conv2D(1, 1, 1)(conv11)
+    
+    fcn = Model(inputs=inputs, outputs=conv12)
+
+    return fcn
+    
+    
+def create_fcn01(input_size):
+    
+    if len(input_size) == 2:
+        inputs = Input((3, input_size[1], input_size[0]))
+    elif len(input_size) == 3:
+        inputs = Input((input_size[2], input_size[1], input_size[0]))
+        
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv1_1')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv1_2')(conv1)
+    pool1 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv2_1')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv2_2')(conv2)
+    pool2 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv3_1')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv3_2')(conv3)
+    pool3 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv4_1')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv4_2')(conv4)
+    pool4 = AveragePooling2D(pool_size=(2, 2), data_format='channels_first')(conv4)
+
+    conv6 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv5_1')(pool4)
+    conv6 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format='channels_first', name='conv5_2')(conv6)
+    
+    up8 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv6), conv4])
+    conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up4_1')(up8)
+    conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up4_2')(conv8)
+
+    up9 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv8), conv3])
+    conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up3_1')(up9)
+    conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up3_2')(conv9)
+
+    up10 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv9), conv2])
+    conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up2_1')(up10)
+    conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up2_2')(conv10)
+
+    up11 = Concatenate(axis=1)([UpSampling2D(size=(2, 2), data_format='channels_first')(conv10), conv1])
+    conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up1_1')(up11)
+    conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first', name='up1_2')(conv11)
+
+    conv12 = Conv2D(1, (1, 1), activation='sigmoid', padding='same', data_format='channels_first', name='conv_fin')(conv11)
+    
+    fcn = Model(inputs=inputs, outputs=conv12)
+
+    return fcn
